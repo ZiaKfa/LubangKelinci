5f70724e8d741f2a3d3d11b614076ca301297af5
Erhaes <150761759+Erhaes@users.noreply.github.com>
2023-11-26 13:13:34 +0700

    ,menambah undangan

--- frontend/script.js
+++ frontend/script.js
@@ -6,3 +6,12 @@ function halaman(angka) {
         window.location.href = 'index.html';
     }
 }
+
+function alertpattern() {
+    var inputpass = document.getElementById('password');
+    if (!inputpass.checkValidity()) {
+        inputpass.setCustomValidity('Pola input tidak sesuai. Masukkan minimal 8 karakter dengan 1 huruf kapital dan 1 angka!');
+        inputpass.alert(inputpass.validationMessage);
+        event.preventDefault();
+    }
+}
